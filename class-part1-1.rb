92cd9fdaa265a0491be5b25df1ac3d4a9ec613ed
purpleice9765 <purpleice9765@msn.com>
2015-09-15 15:05:07 +0800

    class part1 -1

---
+++ class-part1-1.rb
@@ -0,0 +1,56 @@
+class MyCar
+	def initialize(yr,color,model)
+		@yr=yr
+		@color=color
+		@model=model
+		@current_speed = 0
+		
+	end
+	
+	def yr
+		puts "#{@yr}"
+	end
+
+	def color
+		puts "#{@color}"
+	end
+	
+	def model
+		puts "#{@model}"
+	end
+
+	def speed_up(num)
+		@current_speed +=num
+		puts "speed now = #{@current_speed}"
+	end
+
+	def brake(num)
+		@current_speed -=num
+		puts "speed now = #{@current_speed}"
+	end
+
+	def shut
+		@current_speed = 0
+		puts "speed now = #{@current_speed}, it's shut now"
+		
+	end
+
+	def speed_now
+		puts "speed now = #{@current_speed}"
+		
+	end
+
+
+
+end
+
+
+a=MyCar.new(13,15,188)
+
+a.color
+a.yr
+a.model
+a.speed_up(50)
+a.brake(10)
+a.speed_now
+a.shut
