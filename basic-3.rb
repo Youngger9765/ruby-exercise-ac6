ca8a0616a13c71e7f4d19f946db986c8f726bd58
purpleice9765 <purpleice9765@msn.com>
2015-09-15 11:57:21 +0800

    basic-3

---
+++ basic-3.rb
@@ -0,0 +1,13 @@
+movie={
+	a:1999,
+	b:2014,
+	c:2000,
+	d:1934
+}
+
+
+puts "movie[a]: #{movie[:a]} year"
+puts "movie[b]: #{movie[:b]} year"
+puts "movie[c]: #{movie[:c]} year"
+puts "movie[d]: #{movie[:d]} year"
+
